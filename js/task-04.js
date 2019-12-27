b56581dc911e3c488052abda2b6c31bba0bd1c0f
Anastasiia Sereda <nastiasereda.1994@gmail.com>
2019-12-27 22:07:10 +0200

    Added 4 tasks

---
+++ js/task-04.js
@@ -0,0 +1,36 @@
+
+
+class StringBuilder {
+    constructor(string) {
+        this._value = string;
+  }
+  get value() {
+      return this._value
+  }
+  
+  
+  append(string) {
+     this._value = this._value.concat(string);
+  }
+  prepend(string) {
+     this._value = string + this._value;
+   
+  }
+  pad(string) {
+     this._value = string + this._value + string; 
+    
+  }
+}
+
+
+const builder = new StringBuilder(".");
+
+builder.append('^');
+console.log(builder.value); // '.^'
+
+builder.prepend('^');
+console.log(builder.value); // '^.^'
+
+builder.pad('=');
+console.log(builder.value); // '=^.^='
+
