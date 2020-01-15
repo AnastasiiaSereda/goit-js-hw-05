a1287097a450b1b894111f6d2cf29b291e83b6d8
Anastasiia Sereda <nastiasereda.1994@gmail.com>
2020-01-15 17:15:59 +0200

    Checked version

---
+++ js/classwork.js
@@ -0,0 +1,48 @@
+const users = [
+    {
+      name: 'John',
+      age: 100,
+      salary: 1250,
+    },
+    {
+      name: 'Valera',
+      age: 50,
+      salary: 5250,
+    },
+    {
+      name: 'Kolia',
+      age: 20,
+      salary: 7250,
+    },
+    {
+      name: 'Konchita',
+      age: 80,
+      salary: 3530,
+    },
+  ];
+
+//   const reduce =( array, callback, initialValue = array[0]) => {
+//       let accumulator = initialValue;
+//     for (let i= 0; i<array.length; i+=1) {
+//         accumulator = callback(accumulator, array[i]);
+//     }
+//     return accumulator;
+//   };
+// 
+
+
+
+
+//  ------------------  1  ---------------------
+// const totalSalary = reduce( users,(acc, salary) =>) acc + salary. quantity, 0,);
+// console.log(totalSalary);
+
+
+
+
+// ------------------  2  ---------------------
+// const sortedUsers = [...users].sort(userJohn, userValera) => {
+//     return userJohn.rank - userValera.rank;
+// };
+// console.log (sortedUsers);
+
